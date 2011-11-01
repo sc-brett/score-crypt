b09c10d5f0f74c3f489e45ebba7ea9e66a9f74c9
Brett Anderson <bretand@gmail.com>
2011-11-01 15:43:53 -0500

    Initial commit of rails stub files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScoreCrypt::Application.config.secret_token = 'c80a386d63bf5ebc9a4603bc36f037db424fcc9f67db3d625a5689f4b3b69f9146e2c299d60ab1310e624ea01ef1d3a704fabf1f259bdd4e6184c9e7fac97b65'
